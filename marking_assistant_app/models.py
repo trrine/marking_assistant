1831da15fb76fca41371f02861b3021636a7c163
Trine <41973043+trrine@users.noreply.github.com>
2023-09-05 15:45:53 +1000

    Create data models

--- marking_assistant_app/models.py
+++ marking_assistant_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Assignment(models.Model):
+    assignment_number = models.PositiveIntegerField(default=1)
+    subject = models.TextField()
+    total_mark = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Task(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    task_number = models.PositiveIntegerField(default=1)
+    total_mark = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Criteria(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    criteria_number = models.PositiveIntegerField(default=1)
+    description = models.TextField()
+    marks = models.DecimalField(max_digits=5, decimal_places=2)
+    feedback_comment = models.TextField()
