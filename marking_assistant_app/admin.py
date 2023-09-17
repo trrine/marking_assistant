5c924e56969b95c61e9491390a1a40256478929f
Trine <41973043+trrine@users.noreply.github.com>
2023-09-17 13:35:18 +1000

    Add templates

--- marking_assistant_app/admin.py
+++ marking_assistant_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Assignment, Task, Criteria
 
-# Register your models here.
+admin.site.register(Assignment)
+admin.site.register(Task)
+admin.site.register(Criteria)
