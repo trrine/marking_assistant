b0bfecad43c92bb57aab0dba3fbf6684ffd72598
Trine <41973043+trrine@users.noreply.github.com>
2023-09-17 15:45:54 +1000

    Add typescript for add feature

--- marking_assistant_app/static/js/add.js
+++ marking_assistant_app/static/js/add.js
@@ -1,68 +1,31 @@
-// add event listeners for dynamic task and criteria creation
-document.addEventListener("DOMContentLoaded", function() {
-    // add click event listener to the "Add Task" button
-    document.querySelector(".add-task").addEventListener("click", addTask);
-
-    // add click event listener to dynamically created "Add Criteria" buttons
-    document.addEventListener("click", function(event) {
-        // check if the clicked element has the class "add-criteria"
+// Add event listeners for dynamic task and criteria creation
+document.addEventListener("DOMContentLoaded", function () {
+    // Add click event listener to the "Add Task" button
+    var addTaskButton = document.querySelector(".add-task");
+    addTaskButton.addEventListener("click", addTask);
+    // Add click event listener to dynamically created "Add Criteria" buttons
+    document.addEventListener("click", function (event) {
+        // Check if the clicked element has the class "add-criteria"
         if (event.target.classList.contains("add-criteria")) {
-            // call the addCriteria function when an "Add Criteria" button is clicked
+            // Call the addCriteria function when an "Add Criteria" button is clicked
             addCriteria(event.target);
         }
     });
 });
-
 function addTask() {
-    const taskContainer = document.getElementById("task-container");
-    const taskDiv = document.createElement("div");
+    var taskContainer = document.getElementById("task-container");
+    var taskDiv = document.createElement("div");
     taskDiv.className = "task";
-
-    // define HTML structure for task
-    taskDiv.innerHTML = `
-        <hr>
-        <h3>Task Details</h3>
-        <label for="task_number">Task Number:</label>
-        <input type="text" name="task_number[]" required>
-        <label for="task_total_mark">Total Mark for Task:</label>
-        <input type="number" name="task_total_mark[]" required>
-
-        <!-- Criteria details for this task (allow adding multiple criteria) -->
-        <div class="criteria-container">
-            <h4>Criteria Details</h4>
-            <div class="criteria">
-                <label for="criteria_number">Criteria Number:</label>
-                <input type="text" name="criteria_number[]" required>
-                <label for="description">Description:</label>
-                <input type="text" name="description[]" required>
-                <label for="marks">Marks:</label>
-                <input type="number" name="marks[]" required>
-                <label for="feedback_comment">Feedback Comment:</label>
-                <input type="text" name="feedback_comment[]">
-            </div>
-        </div>
-        <button type="button" class="add-criteria">Add Criteria</button>
-    `;
-
+    // Define HTML structure for task
+    taskDiv.innerHTML = "\n        <hr>\n        <h3>Task Details</h3>\n        <label for=\"task_number\">Task Number:</label>\n        <input type=\"text\" name=\"task_number[]\" required>\n        <label for=\"task_total_mark\">Total Mark for Task:</label>\n        <input type=\"number\" name=\"task_total_mark[]\" required>\n\n        <div class=\"criteria-container\">\n            <h4>Criteria Details</h4>\n            <div class=\"criteria\">\n                <label for=\"criteria_number\">Criteria Number:</label>\n                <input type=\"text\" name=\"criteria_number[]\" required>\n                <label for=\"description\">Description:</label>\n                <input type=\"text\" name=\"description[]\" required>\n                <label for=\"marks\">Marks:</label>\n                <input type=\"number\" name=\"marks[]\" required>\n                <label for=\"feedback_comment\">Feedback Comment:</label>\n                <input type=\"text\" name=\"feedback_comment[]\">\n            </div>\n        </div>\n        <button type=\"button\" class=\"add-criteria\">Add Criteria</button>\n    ";
     taskContainer.appendChild(taskDiv);
 }
-
 function addCriteria(button) {
-    const criteriaContainer = button.parentElement.querySelector(".criteria-container");
-    const criteriaDiv = document.createElement("div");
+    var _a;
+    var criteriaContainer = (_a = button.parentElement) === null || _a === void 0 ? void 0 : _a.querySelector(".criteria-container");
+    var criteriaDiv = document.createElement("div");
     criteriaDiv.className = "criteria";
-
-    // define HTML structure for criteria
-    criteriaDiv.innerHTML = `
-        <label for="criteria_number">Criteria Number:</label>
-        <input type="text" name="criteria_number[]" required>
-        <label for="description">Description:</label>
-        <input type="text" name="description[]" required>
-        <label for="marks">Marks:</label>
-        <input type="number" name="marks[]" required>
-        <label for="feedback_comment">Feedback Comment:</label>
-        <input type="text" name="feedback_comment[]">
-    `;
-
+    // Define HTML structure for criteria
+    criteriaDiv.innerHTML = "\n        <label for=\"criteria_number\">Criteria Number:</label>\n        <input type=\"text\" name=\"criteria_number[]\" required>\n        <label for=\"description\">Description:</label>\n        <input type=\"text\" name=\"description[]\" required>\n        <label for=\"marks\">Marks:</label>\n        <input type=\"number\" name=\"marks[]\" required>\n        <label for=\"feedback_comment\">Feedback Comment:</label>\n        <input type=\"text\" name=\"feedback_comment[]\">\n    ";
     criteriaContainer.appendChild(criteriaDiv);
 }
---
+++ marking_assistant_app/static/js/add.ts
@@ -0,0 +1,68 @@
+// Add event listeners for dynamic task and criteria creation
+document.addEventListener("DOMContentLoaded", () => {
+    // Add click event listener to the "Add Task" button
+    const addTaskButton = document.querySelector(".add-task") as HTMLButtonElement;
+    addTaskButton.addEventListener("click", addTask);
+
+    // Add click event listener to dynamically created "Add Criteria" buttons
+    document.addEventListener("click", (event) => {
+        // Check if the clicked element has the class "add-criteria"
+        if ((event.target as HTMLElement).classList.contains("add-criteria")) {
+            // Call the addCriteria function when an "Add Criteria" button is clicked
+            addCriteria(event.target as HTMLButtonElement);
+        }
+    });
+});
+
+function addTask() {
+    const taskContainer = document.getElementById("task-container") as HTMLDivElement;
+    const taskDiv = document.createElement("div");
+    taskDiv.className = "task";
+
+    // Define HTML structure for task
+    taskDiv.innerHTML = `
+        <hr>
+        <h3>Task Details</h3>
+        <label for="task_number">Task Number:</label>
+        <input type="text" name="task_number[]" required>
+        <label for="task_total_mark">Total Mark for Task:</label>
+        <input type="number" name="task_total_mark[]" required>
+
+        <div class="criteria-container">
+            <h4>Criteria Details</h4>
+            <div class="criteria">
+                <label for="criteria_number">Criteria Number:</label>
+                <input type="text" name="criteria_number[]" required>
+                <label for="description">Description:</label>
+                <input type="text" name="description[]" required>
+                <label for="marks">Marks:</label>
+                <input type="number" name="marks[]" required>
+                <label for="feedback_comment">Feedback Comment:</label>
+                <input type="text" name="feedback_comment[]">
+            </div>
+        </div>
+        <button type="button" class="add-criteria">Add Criteria</button>
+    `;
+
+    taskContainer.appendChild(taskDiv);
+}
+
+function addCriteria(button: HTMLButtonElement) {
+    const criteriaContainer = button.parentElement?.querySelector(".criteria-container") as HTMLDivElement;
+    const criteriaDiv = document.createElement("div");
+    criteriaDiv.className = "criteria";
+
+    // Define HTML structure for criteria
+    criteriaDiv.innerHTML = `
+        <label for="criteria_number">Criteria Number:</label>
+        <input type="text" name="criteria_number[]" required>
+        <label for="description">Description:</label>
+        <input type="text" name="description[]" required>
+        <label for="marks">Marks:</label>
+        <input type="number" name="marks[]" required>
+        <label for="feedback_comment">Feedback Comment:</label>
+        <input type="text" name="feedback_comment[]">
+    `;
+
+    criteriaContainer.appendChild(criteriaDiv);
+}
